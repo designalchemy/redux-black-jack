d0de80b7c93403f2cc187b4f96a7831d6b983a58
Luke Robertson <luke.robertson@korelogic.co.uk>
2018-06-10 23:25:17 +0100

    added emjys

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -10,13 +10,47 @@ const styles = {
     borderRadius: 6,
     marginRight: 20,
     backgroundColor: 'white',
-    color: 'red'
+    color: 'red',
+    position: 'relative',
+    textTransform: 'uppercase'
+  },
+  topLeft: {
+    position: 'absolute',
+    left: 5,
+    top: 5
+  },
+  bottomRight: {
+    position: 'absolute',
+    right: 5,
+    bottom: 5
   }
 }
 
-const Card = props => (
-  <div className={props.classes.container}>{props.card}</div>
-)
+const symbol = letter => {
+  switch (letter) {
+    case 'h':
+      return '♥️'
+    case 'd':
+      return '♦️'
+    case 's':
+      return '♠️'
+    case 'c':
+      return '♣️'
+    default:
+      return '?'
+  }
+}
+
+const Card = props => {
+  const card = props.card.split('-')
+  const char = `${symbol(card[0])}${card[1]}`
+  return (
+    <div className={props.classes.container}>
+      <div className={props.classes.topLeft}>{char}</div>
+      <div className={props.classes.bottomRight}>{char}</div>
+    </div>
+  )
+}
 
 Card.propTypes = {
   classes: PropTypes.object,
